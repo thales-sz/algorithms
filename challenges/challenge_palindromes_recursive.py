2792e414ec6bfb6e89c5a45fc08a6308682a4f0b
Thales <thales.souz@outlook.com>
2023-02-16 16:58:49 -0300

    feat: is_palindrome_recursive implemented, req 03

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,4 +3,6 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
     if word[low_index] != word[high_index]:
         return False
+    if high_index == -1:
+        return True
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
