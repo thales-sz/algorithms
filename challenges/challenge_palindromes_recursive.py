2519d05853a9238255dee44cd1be1fb04300f0c1
Thales <thales.souz@outlook.com>
2023-02-16 16:57:30 -0300

    feat: is_palindrome test pass on 2/3, req 03

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if word == "":
+    if not word:
         return False
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
