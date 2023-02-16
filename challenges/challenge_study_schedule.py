7e8cca1097e2a7b52372d39bd0762f2c973386bc
Thales <thales.souz@outlook.com>
2023-02-16 15:50:41 -0300

    feat: study_schedule implemented - req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    if target_time == 0 or target_time is not None:
+        for period in permanence_period:
+            if type(period[0]) == int and type(period[1]) == int:
+                if period[0] <= target_time <= period[1]:
+                    counter += 1
+            else:
+                return None
+        return counter
+    else:
+        return None
+
+
+mock = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), (6, 7)]
