be3956488ed75dfed5fb9cddd3138725689b1bb5
Thales <thales.souz@outlook.com>
2023-02-16 15:51:53 -0300

    feat: study_schedule implemented

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,9 @@
 def study_schedule(permanence_period, target_time):
     counter = 0
     if target_time == 0 or target_time is not None:
-        for period in permanence_period:
-            if type(period[0]) == int and type(period[1]) == int:
-                if period[0] <= target_time <= period[1]:
+        for enter, out in permanence_period:
+            if type(enter) == int and type(out) == int:
+                if enter <= target_time <= out:
                     counter += 1
             else:
                 return None
